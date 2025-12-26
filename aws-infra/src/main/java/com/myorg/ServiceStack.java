1f057ab43d5b92e4b82c041d626a7af76aeb3a3b
Tiago de Almeida Santos <tiagoalmeidasantos3412@gmail.com>
2025-12-26 12:22:36 -0300

    feat: configura grupos de log e log drivers para serviços Fargate e ajusta a contagem desejada de instâncias.

--- aws-infra/src/main/java/com/myorg/ServiceStack.java
+++ aws-infra/src/main/java/com/myorg/ServiceStack.java
@@ -6,12 +6,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 import software.amazon.awscdk.Fn;
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
 import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.LogDriver;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.amazon.awscdk.services.logs.RetentionDays;
 
 public class ServiceStack extends Stack {
         public ServiceStack(final Construct scope, final String id, final Cluster cluster) {
@@ -27,15 +32,30 @@ public class ServiceStack extends Stack {
 
                 autenticate.put("SPRING_DATASOURCE_USERNAME", "admin");
                 autenticate.put("SPRING_DATASOURCE_PASSWORD", Fn.importValue("pedidos-db-senha"));
+
+                LogGroup logGroupOrder = LogGroup.Builder.create(this, "PedidosMsLogGroup")
+                                .logGroupName("PedidosMsLog")
+                                .removalPolicy(RemovalPolicy.DESTROY)
+                                .retention(RetentionDays.ONE_WEEK)
+                                .build();
+                LogGroup logGroupPayments = LogGroup.Builder.create(this, "PaymentsMsLogGroup")
+                                .logGroupName("PaymentsMsLog")
+                                .removalPolicy(RemovalPolicy.DESTROY)
+                                .retention(RetentionDays.ONE_WEEK)
+                                .build();
                 ApplicationLoadBalancedFargateService.Builder.create(this, "Fargate-ms-pedidos")
                                 .cluster(cluster)
                                 .cpu(256)
-                                .desiredCount(6)
+                                .desiredCount(3)
                                 .listenerPort(8080)
                                 .assignPublicIp(true)
                                 .taskImageOptions(
                                                 ApplicationLoadBalancedTaskImageOptions.builder()
                                                                 .environment(autenticate)
+                                                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                                                                .logGroup(logGroupOrder)
+                                                                                .streamPrefix("PedidosMS")
+                                                                                .build()))
                                                                 .image(ContainerImage
                                                                                 .fromRegistry("ctiagosantos/order-ms"))
                                                                 .containerPort(8080)
@@ -54,12 +74,16 @@ public class ServiceStack extends Stack {
                 ApplicationLoadBalancedFargateService.Builder.create(this, "Fargate-ms-pagamentos")
                                 .cluster(cluster)
                                 .cpu(256)
-                                .desiredCount(6)
+                                .desiredCount(3)
                                 .listenerPort(8080)
                                 .assignPublicIp(true)
                                 .taskImageOptions(
                                                 ApplicationLoadBalancedTaskImageOptions.builder()
                                                                 .environment(autenticatePagamentos)
+                                                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                                                                .logGroup(logGroupPayments)
+                                                                                .streamPrefix("PaymentsMS")
+                                                                                .build()))
                                                                 .image(ContainerImage.fromRegistry(
                                                                                 "ctiagosantos/payment-ms"))
                                                                 .containerPort(8080)
