2c95fea9b5ef652364fd5789516bfeb3fe484da6
Tiago de Almeida Santos <tiagoalmeidasantos3412@gmail.com>
2025-12-28 01:09:59 -0300

    feat: adiciona integração com RabbitMQ para eventos de pagamento e atualiza dependências do Feign para OpenFeign.

---
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/amqp/PaymentAmqpConfig.java
@@ -0,0 +1,30 @@
+package br.com.alurafood.pagamentos.amqp;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+
+@Configuration
+public class PaymentAmqpConfig {
+
+    @Bean
+    public Queue createQueue() {
+        return QueueBuilder.nonDurable("payments.completed").build();
+    }
+
+    @Bean
+    public RabbitAdmin createRabbitAdmin(@NonNull ConnectionFactory conn) {
+        return new RabbitAdmin(conn);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> initAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+}
